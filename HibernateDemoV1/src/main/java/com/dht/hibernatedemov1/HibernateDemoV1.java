1904b28b253ad01fc1d0d25a868ddc4f4eeb929d
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2023-06-23 17:02:10 +0700

    add hibernate

---
+++ HibernateDemoV1/src/main/java/com/dht/hibernatedemov1/HibernateDemoV1.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.dht.hibernatedemov1;
+
+import com.dht.pojo.Category;
+import com.dht.repository.ProductRepository;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author admin
+ */
+public class HibernateDemoV1 {
+
+    public static void main(String[] args) {
+        try (Session s = HibernateUtils.getFactory().openSession()) {
+            Query q = s.createNamedQuery("Category.findById", Category.class);
+            q.setParameter("id", 1);
+            List<Category> cates = q.getResultList();
+            cates.forEach(c -> System.out.println(c.getName()));
+        }
+//        Map<String, String> params = new HashMap<>();
+////        params.put("toPrice", "50000000");
+//        params.put("cateId", "1");
+//        
+//        ProductRepository pre = new ProductRepository();
+//        pre.getProducts(params).forEach(p -> System.out.printf("%d - %s - %.1f\n", p.getId(), p.getName(), p.getPrice()));
+    }
+}
